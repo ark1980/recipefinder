79b4a94869f6f17902a446d56fa2ff64fbe74356
Ali <ar.k@outlook.com>
2017-08-29 11:44:12 -0700

    create searchRecipies component to fetch data from API

---
+++ src/components/SearchRecipies.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Button, Form, FormControl, FormGroup, ControlLabel} from 'react-bootstrap';
+
+class SearchRecipies extends Component {
+    constructor() {
+        super();
+        this.state = {
+            ingredients: "",
+            dish: ""
+        }
+    }
+
+    search() {
+        let { ingredients, dish } = this.state;
+        const url = `http://www.recipepuppy.com/api/?i=${ingredients}&q=${dish}`;
+        console.log('url', url, 'state', this.state);
+    }
+
+
+    render() {
+        return(
+            <Form inline>
+                <FormGroup>
+                    <ControlLabel>Ingredients</ControlLabel>
+                    <FormControl 
+                        type="text" 
+                        placeholder="Galrli, Chicken" 
+                        className="input" 
+                        onChange={event => this.setState({ingredients: event.target.value})}
+                        />
+                </FormGroup>
+                
+                <FormGroup>
+                    <ControlLabel>Dish</ControlLabel>
+                    <FormControl 
+                        type="text" 
+                        placeholder="Food" 
+                        className="input"
+                        onChange={event => this.setState({dish: event.target.value})}
+                        />
+                </FormGroup>
+                <Button
+                    className="btn btn-dark"
+                    onClick={() => this.search()}
+                    >
+                    Submit</Button>
+            </Form>
+        )
+    }
+}
+
+export default SearchRecipies;
